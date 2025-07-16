0e244f23136f449b5bb99392ddd18cb010fa0f2f
ASHIMA TIWARI <ashimatiwari2004@gmail.com>
2025-07-16 17:35:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".texted", {
+    strings: ["Frontend Developer", "Web Developer", "Software Tester", "FullStack Developer"],
+    typeSpeed: 90,
+    backSpeed: 70,
+    backDelay: 500,
+    loop: true
+});
+
+
+
